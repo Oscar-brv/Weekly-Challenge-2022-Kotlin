a2267c9e48edf018e0dba8112300145c63523313
Oscar Bravo <oscar.bravo@usach.cl>
2022-08-28 18:38:52 -0400

    Python|Solución Reto #33
    Solo Update de la Rama

---
+++ Python/Challenge33.py
@@ -0,0 +1,32 @@
+# 
+#   Reto #33CICLO
+#   CICLO SEXAGENARIO CHINO
+#   Fecha publicación enunciado: 15/08/22
+#   Fecha publicación resolución: 22/08/22
+#   Dificultad: MEDIA
+#  
+#   Enunciado: Crea un función, que dado un año, indique el elemento y animal correspondiente
+#   en el ciclo sexagenario del zodíaco chino.
+#   - Información: https://www.travelchinaguide.com/intro/astrology/60year-cycle.htm
+#   - El ciclo sexagenario se corresponde con la combinación de los elementos madera,
+#     fuego, tierra, metal, agua y los animales rata, buey, tigre, conejo, dragón, serpiente,
+#     caballo, oveja, mono, gallo, perro, cerdo (en este orden).
+#   - Cada elemento se repite dos años seguidos.
+#   - El último ciclo sexagenario comenzó en 1984 (Madera Rata).
+#  
+#   Información adicional:
+#   - Usa el canal de nuestro Discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar 
+#     ayuda a la comunidad.
+#   - Tienes toda la información sobre los retos semanales en https://retosdeprogramacion.com/semanales2022.
+#  
+# 
+
+ELEMENTS = ["Wood", "Fire", "Earth", "Metal", "Water"]
+ANIMALS = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep", "Monkey", "Rooster", "Dog", "Pig"]
+
+
+def year_of_cycle(year: int):
+    print(1984-1924)
+
+year_of_cycle(2022)
+    
